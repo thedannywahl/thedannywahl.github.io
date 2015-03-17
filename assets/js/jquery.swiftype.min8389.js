bbb0515d819b28255d943ad983340c373f41bf6a
Danny Wahl <danny@iyware.com>
2015-03-17 20:19:12 +0800

    Blog update at 2015-03-17 12:19:12

--- assets/js/jquery.swiftype.min8389.js
+++ assets/js/jquery.swiftype.min8389.js
@@ -10,20 +10,20 @@ var stSearch = function() {var query=document.getElementById("st-search-input").
 				var body = '<div class="text"><p>' + item['body'] + '</p></div>';
 				return '<article>' + header + body + '</article>';
 			};
-			$( '<li class="nav-search-form"><form action="#" onsubmit="stSearch();return false;" autocomplete="on"><label id="nav-search-label" for="st-search-input">Search:</label><input id="search_submit" value="Search" type="submit"><input id="st-search-input-nav" name="search" type="text" placeholder="search https://iyware.com"></form></li>' ).insertAfter( ".nav-search" );
-			$("#st-search-input-nav").swiftype({
+			$( '<li class="nav-search-form"><form action="#" onsubmit="stSearch();return false;" autocomplete="on"><label id="nav-search-label" for="st-search-input">Search:</label><input id="search_submit" value="Search" type="submit"><input id="st-search-input" name="search" type="text" placeholder="search https://iyware.com"></form></li>' ).insertAfter( ".nav-search" );
+			$("#st-search-input").swiftype({
 				engineKey: "sg3kn6rAnozjxvM4k8nk"
 			});
-			$("#st-search-input-nav").swiftypeSearch({
+			$("#st-search-input").swiftypeSearch({
 				renderFunction: stRenderFunction,
 				perPage: 5,
 				engineKey: "sg3kn6rAnozjxvM4k8nk",
 				resultContainingElement: "#st-results-container"
 			});
-			$("#st-search-input").swiftype({
+			$("#st-search-input2").swiftype({
 				engineKey: "sg3kn6rAnozjxvM4k8nk"
 			});
-			$("#st-search-input").swiftypeSearch({
+			$("#st-search-input2").swiftypeSearch({
 				renderFunction: stRenderFunction,
 				perPage: 5,
 				engineKey: "sg3kn6rAnozjxvM4k8nk",
