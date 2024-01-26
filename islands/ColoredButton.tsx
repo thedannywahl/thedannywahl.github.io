d2440c2ace6bf3adf0b2386afde88fc120834dda
Danny Wahl <danny@iywahl.com>
2024-01-25 21:16:45 -0700

    prebuilt islands

---
+++ islands/ColoredButton.tsx
@@ -0,0 +1,15 @@
+import { JSX } from "preact";
+import { IS_BROWSER } from "$fresh/runtime.ts";
+
+export default function ColoredButton(
+  props: JSX.HTMLAttributes<HTMLButtonElement>,
+) {
+  return (
+    <button
+      {...props}
+      disabled={!IS_BROWSER || props.disabled}
+      type={props.type ?? "button"}
+      class="px-3 py-2 bg-blue-200 text-blue-800 rounded hover:bg-blue-300 active:bg-blue-400"
+    />
+  );
+}
