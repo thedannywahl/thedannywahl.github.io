b9e3067335d07ea4873a7b4ef2e6eab98318e09d
Danny Wahl <danny@iywahl.com>
2024-01-27 14:08:58 -0700

    Deploying from Deno

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -5,7 +5,11 @@
 import * as $_404 from "./routes/_404.tsx";
 import * as $_app from "./routes/_app.tsx";
 import * as $about from "./routes/about.tsx";
+import * as $blog from "./routes/blog.tsx";
+import * as $blog_slug_ from "./routes/blog/[slug].tsx";
+import * as $blog_feed from "./routes/blog/feed.ts";
 import * as $blog_index from "./routes/blog/index.tsx";
+import * as $feed from "./routes/feed.ts";
 import * as $index from "./routes/index.tsx";
 import * as $projects from "./routes/projects.tsx";
 import * as $Button from "./islands/Button.tsx";
@@ -13,6 +17,7 @@ import * as $Carousel from "./islands/Carousel.tsx";
 import * as $Code from "./islands/Code.tsx";
 import * as $ColoredButton from "./islands/ColoredButton.tsx";
 import * as $Input from "./islands/Input.tsx";
+import * as $PostCard from "./islands/PostCard.tsx";
 import { type Manifest } from "$fresh/server.ts";
 
 const manifest = {
@@ -20,7 +25,11 @@ const manifest = {
     "./routes/_404.tsx": $_404,
     "./routes/_app.tsx": $_app,
     "./routes/about.tsx": $about,
+    "./routes/blog.tsx": $blog,
+    "./routes/blog/[slug].tsx": $blog_slug_,
+    "./routes/blog/feed.ts": $blog_feed,
     "./routes/blog/index.tsx": $blog_index,
+    "./routes/feed.ts": $feed,
     "./routes/index.tsx": $index,
     "./routes/projects.tsx": $projects,
   },
@@ -30,6 +39,7 @@ const manifest = {
     "./islands/Code.tsx": $Code,
     "./islands/ColoredButton.tsx": $ColoredButton,
     "./islands/Input.tsx": $Input,
+    "./islands/PostCard.tsx": $PostCard,
   },
   baseUrl: import.meta.url,
 } satisfies Manifest;
