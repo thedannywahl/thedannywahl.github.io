a92df59292b54486d4a0c25a4df9dcc58ad8c6a3
Danny Wahl <danny@iywahl.com>
2024-01-28 17:10:55 -0700

    Update Header.tsx
    add border to break nav from content on small viewport

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,7 +6,7 @@ export default function Header() {
     { page: "Blog", href: "/blog" },
   ];
   return (
-    <nav class="flex-none py-4 px-4 sm:py-6 sm:px-8 flex flex-col sm:flex-row lg:mb-8 lg:rounded-xl shadow dark:shadow-none bg-[#90bdbc] dark:bg-gray-800 dark:lg:bg-transparent">
+    <nav class="flex-none py-4 px-4 sm:py-6 sm:px-8 flex flex-col sm:flex-row lg:mb-8 lg:rounded-xl shadow dark:shadow-none bg-[#90bdbc] dark:bg-gray-800 dark:lg:bg-transparent border-b-2 lg:border-0 dark:border-0">
       <div class="flex items-center flex-1">
         <div class="text-4xl sm:text-2xl font-thin text-white dark:text-[#6ba7a5] mx-auto sm:mx-0 pb-4 sm:pb-0">
           iyWahl
